99533b099d44fa6e697df9f81680ab790a3bd20a
Monis9451 <mianmonishussain786@gmail.com>
2024-07-29 12:59:18 +0500

    practicing EJS tags

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const path = require('path')
 
 app.set("view engine", "ejs")
+app.set("viewa",path.join(__dirname, "/views"))
 
 app.get('/', (req, res)=>{
-    res.send('This is home')
+    res.render("home.ejs")
 })
 
 app.listen(port, ()=>{

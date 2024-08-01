1ca40fc4c875da58d23ab2fbbd02b5928ba46b7f
Monis9451 <mianmonishussain786@gmail.com>
2024-08-01 11:35:58 +0500

    practicing restful APIs

--- restful/index.js
+++ restful/index.js
@@ -7,7 +7,36 @@ app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,"views"))
 
-app.set(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"public")))
+
+let posts = [
+    {
+        username: "Monis",
+        content: "I love coding!"
+    },
+    {
+        username: "Hussain",
+        content: "Hard work is important to achieve success!"
+    },
+    {
+        username: "Mohsin",
+        content: "I got my first internship"
+    }
+]
+
+app.get('/posts',(req, res)=>{
+    res.render("index.ejs", {posts})
+})
+
+app.get('/posts/new',(req, res)=>{
+    res.render("new.ejs")
+})
+
+app.post("/posts",(req, res)=>{
+    let {username, content} = req.body
+    posts.push({username, content})
+    res.redirect("/posts")
+})
 
 app.listen(port, ()=>{
     console.log(`listening to port: ${port}`)

a4c19ecf7c7141d03919c9f6f803a9b9a80be1fd
Monis9451 <mianmonishussain786@gmail.com>
2024-08-02 10:54:26 +0500

    Making post practice which is viewable and editable

--- restful/index.js
+++ restful/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 8080
 const path = require("path")
+const {v4: uuidv4} = require("uuid")
 app.use(express.urlencoded({extended:true}))
 
 app.set('view engine','ejs')
@@ -11,14 +12,17 @@ app.use(express.static(path.join(__dirname,"public")))
 
 let posts = [
     {
+        id: uuidv4(),
         username: "Monis",
         content: "I love coding!"
     },
     {
+        id: uuidv4(),
         username: "Hussain",
         content: "Hard work is important to achieve success!"
     },
     {
+        id: uuidv4(),
         username: "Mohsin",
         content: "I got my first internship"
     }
@@ -32,9 +36,27 @@ app.get('/posts/new',(req, res)=>{
     res.render("new.ejs")
 })
 
+app.get('/posts/:id',(req, res)=>{
+    let {id} = req.params
+    let post = posts.find((p)=> id ===p.id)
+    res.render("show.ejs",{post})
+})
+
+app.get
+
+app.patch('/posts/:id',(req, res)=>{
+    let {id} = req.params
+    let newContent = req.body.content
+    let post = posts.find((p)=> id ===p.id)
+    post.content = newContent
+    console.log(newContent)
+    res.send("patch request working")
+})
+
 app.post("/posts",(req, res)=>{
     let {username, content} = req.body
-    posts.push({username, content})
+    let id = uuidv4()
+    posts.push({id, username, content})
     res.redirect("/posts")
 })
 

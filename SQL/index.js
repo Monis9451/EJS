24bd9d0330c1df137b266d8f77bc08ce36c15fd0
Monis9451 <mianmonishussain786@gmail.com>
2024-08-14 11:24:49 +0500

    Create a Add route to add new user in database and create a delete route to delete a user from database

--- SQL/index.js
+++ SQL/index.js
@@ -5,10 +5,14 @@ const app = express()
 const port = 8080
 const path = require("path")
 const { count } = require("console")
+const override = require("method-override")
+const { constrainedMemory } = require("process")
+const { v4: uuidv4 } = require('uuid')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,"public")))
+app.use(override("_method"))
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -58,12 +62,98 @@ app.get('/user/:id/edit', (req, res)=>{
   try{
     connection.query(q, (err, result)=>{
       if(err) throw err
+      result = result[0]
+      console.log(result)
+      res.render('edit', {result})
     })
   }
   catch(err){
     console.log(err)
   }
-  res.render('edit')
+})
+
+//Database update
+app.patch('/user/:id',(req, res)=>{
+  let {id} = req.params
+  let {password: formPassword, username: newUsername} = req.body
+  let q = `SELECT * FROM user WHERE id = '${id}'`
+  try{
+    connection.query(q, (err, result)=>{
+      if(err) throw err
+      result = result[0]
+      if(formPassword != result.password){
+        res.send(`Wrong Password`)
+      }else{
+        let q2 = `UPDATE user SET username='${newUsername}' WHERE id='${result.id}'`
+        connection.query(q2, (err, result)=>{
+          if(err) throw err
+          res.redirect('http://localhost:8080/user')
+        })
+      }
+    })
+  }
+  catch(err){
+    console.log(err)
+  }
+})
+
+//Add a user
+app.get('/add', (req, res)=>{
+  res.render('add')
+})
+
+//Adding new user data in database
+app.post('/add',(req, res)=>{
+  let id =  uuidv4()
+  let {username, email, password} = req.body
+  let q = `INSERT INTO user (id, username, email, password) VALUES (?, ?, ?, ?)`
+  let data = [id, username, email, password]
+  try{
+    connection.query(q, data, (err, result)=>{
+      if (err) throw err
+      res.redirect('http://localhost:8080')
+    } )
+  }
+  catch(err){
+    console.log(err)
+  }
+})
+
+//Delete route
+app.get('/user/delete/:id',(req, res)=>{
+  let {id} = req.params
+  res.render('delete',{id})
+})
+
+app.delete('/user/delete/:id',(req, res)=>{
+  let {id} = req.params
+  let{password} = req.body
+  let q = `SELECT * FROM user WHERE id='${id}'`
+  try{
+    connection.query(q,(err, result)=>{
+      if(err) throw err
+      result = result[0]
+      if(password != result.password)
+      {
+        res.send("Incorrect Password")
+      }
+      else{
+        let q2 = `DELETE FROM user WHERE id='${id}'`
+        try{
+          connection.query(q2,(err, result)=>{
+            if(err) throw err
+            res.redirect('http://localhost:8080')
+          })
+        }
+        catch(err){
+          console.log(err)
+        }
+      }
+    })
+  }
+  catch(err){
+    console.log(err)
+  }
 })
 
   // Inserting new data

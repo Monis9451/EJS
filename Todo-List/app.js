517b26ad2092ed20c369ab6c962f97fa9d561437
Monis9451 <mianmonishussain786@gmail.com>
2024-08-15 09:37:14 +0500

    Create a Add button and connection with database

--- Todo-List/app.js
+++ Todo-List/app.js
@@ -2,28 +2,66 @@ const express = require("express")
 const app = express()
 const port = 8080
 const path = require("path")
+const mysql = require("mysql2")
 app.set("veiw engine", "ejs")
 app.set("veiws", path.join(__dirname,"/views"))
 app.use(express.static(path.join(__dirname,"public")))
-app.use(express(express.urlencoded({extended:true})))
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+const { v4: uuidv4 } = require('uuid')
 
-let list = ["Want to do some chores", "Do the assignment", "Go to market"]
-let doneWork = ["Final year project"]
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'todoList',
+    password: 'Monis9451..com'
+  });
 
 app.listen(port,()=>{
     console.log("Port", port, "is listening")
 })
 app.get("/",(req, res)=>{
-    res.render('home.ejs',{list,doneWork})
+    let result1, result2
+    let q1 = `SELECT * FROM data`
+    let q2 = `SELECT * FROM completed`
+    try{
+        connection.query(q1,(err, result1)=>{
+            if(err) throw err
+            console.log(result1)
+            
+            try{
+                connection.query(q2,(err, result2)=>{
+                    if(err) throw err
+                    console.log(result2)
+                    res.render('home.ejs', {result1,result2})
+                })
+            }
+            catch(err){
+                console.log(err)
+            }
+                })
+    }
+    catch(err){
+        console.log(err)
+    }
 })
 
 app.get("/add",(req, res)=>{
-    console.log("This is an art router")
     res.render('add.ejs')
 })
 
-let addBtn = document.querySelector(".add")
-addBtn.addEventListener("click", ()=>{
-    res.render('/add')
+app.post("/add",(req, res)=>{
+    let {task} = req.body
+    let id = uuidv4()
+    let q = `INSERT INTO data (id, task) VALUES (?, ?)`
+    let combine = [id, task]
+    try{
+        connection.query(q,combine,(err, result)=>{
+            if(err) throw err
+            res.redirect('http://localhost:8080/')
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
 })
---
+++ Todo-List/schema.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE data(
+--     id VARCHAR(50) PRIMARY KEY,
+--     task VARCHAR(200)
+-- )
+
+-- CREATE TABLE completed(
+--     id VARCHAR(50) PRIMARY KEY,
+--     task VARCHAR(200)
+-- )
+
+-- INSERT INTO data (id, task) VALUES
+-- ('101','Final year project'),
+-- ('102', 'Have to complete the course');
+
+-- INSERT INTO completed (id, task) VALUES
+-- ('103','DSA revision'),
+-- ('104', 'Have to refill the car');
